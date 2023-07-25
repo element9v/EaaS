726d644cbdb81021815d65eabc78edd0715862fa
AQ Glass <element9v@gmail.com>
2023-07-25 13:36:56 -0700

    installer

--- install.sh
+++ install.sh
@@ -6,12 +6,17 @@
 ## https://github.com/element9v/eaas
 ## dns-everything
 
+PIP_FLAGS="--force-reinstall" 
+#PIP_FLAGS="" 
 
 # python3 venv
-apt install python
+apt install binutils python rust
 python3 -m venv installer/
-source installer/bin/activate
-python3 -m pip install --upgrade pip
-#python3 -m pip install pynacl
-#pip3 install rust
-pip3 install ansible
+. ./installer/bin/activate
+pip3 install --upgrade pip
+pip3 install ${PIP_FLAGS} numpy
+pip3 install ${PIP_FLAGS} wheel
+pip3 install ${PIP_FLAGS} discord.py[voice] pynacl
+pip3 install ${PIP_FLAGS} cargo
+pip3 install ${PIP_FLAGS} rust
+pip3 install ${PIP_FLAGS} ansible
