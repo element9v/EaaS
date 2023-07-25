9ba8d38052b80286135a80c2066a709e044add98
AQ Glass <element9v@gmail.com>
2023-07-25 12:47:20 -0700

    installer

--- install.sh
+++ install.sh
@@ -13,5 +13,5 @@ python3 -m venv installer/
 source installer/bin/activate
 python3 -m pip install --upgrade pip
 #python3 -m pip install pynacl
-python3 -m pip install rust
-python3 -m pip install ansible
+#pip3 install rust
+pip3 install ansible
